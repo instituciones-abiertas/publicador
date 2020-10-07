0234d3ea7653c1a58a99309c5bf97325c82309be
José Luis Di Biase <josx@interorganic.com.ar>
2020-10-07 18:05:00 -0300

    Google and dropbox: Credentials and path support
    Signed-off-by: José Luis Di Biase <josx@interorganic.com.ar>

--- publicador/dropboxapi/dropboxapi.py
+++ publicador/dropboxapi/dropboxapi.py
@@ -1,14 +1,14 @@
 from ..abstract import Strategy
 import dropbox, os
-from decouple import config
-
-DROPBOX_TOKEN = config('DROPBOX_TOKEN', default='')
 
 class DropboxApi(Strategy):
+
+    def __init__(self, token, path) -> None:
+        self.token = token
+        self.path = path
+
     def publicacion(self, archivo: str) -> str:
-        dropbox_path= '/example'
         file_basename = os.path.basename(archivo)
-        client = dropbox.Dropbox(DROPBOX_TOKEN, scope=['files.content.write'])
-        client.files_upload(open(archivo, "rb").read(), os.path.join(dropbox_path, file_basename), mode=dropbox.files.WriteMode.overwrite)
-        print("[UPLOADED] {}".format(archivo))
-        return 'Ok'
+        client = dropbox.Dropbox(self.token, scope=['files.content.write'])
+        client.files_upload(open(archivo, "rb").read(), os.path.join(self.path, file_basename), mode=dropbox.files.WriteMode.overwrite)
+        return "[UPLOADED] {}".format(archivo)
--- publicador/googledrive/googledrive.py
+++ publicador/googledrive/googledrive.py
@@ -2,32 +2,20 @@ from __future__ import print_function
 
 import os
 from  ..abstract import Strategy
-from googleapiclient import discovery
-from googleapiclient.http import MediaFileUpload
-from httplib2 import Http
-from oauth2client import file, client, tools
+from cd_drive import Drive
 import mimetypes
 
 class GoogleDrive(Strategy):
+
+    def __init__(self, credentials_file, path) -> None:
+        self.credentials_file = credentials_file
+        self.path = path
+
     def publicacion(self, archivo: str) -> str:
-        res = self.upload(archivo)
-        return res
-
-    def upload(self, archivo: str):
-        global file
-        SCOPES = 'https://www.googleapis.com/auth/drive'
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        store = file.Storage(os.path.join(dir_path, 'storage.json'))
-        creds = store.get()
-        if not creds or creds.invalid:
-            flow = client.flow_from_clientsecrets(os.path.join(dir_path,'client_id.json'), SCOPES)
-            creds = tools.run_flow(flow, store)
-
-        drive_service = discovery.build('drive', 'v3', http=creds.authorize(Http()))
-
         mimetypes.init()
-        file_mimetype = mimetypes.guess_type(archivo)[0] or 'text/plain'
-        file_metadata = {'name': os.path.basename(archivo) }
-        media = MediaFileUpload(archivo, mimetype=file_mimetype)
-        file = drive_service.files().create(body=file_metadata, media_body=media, fields='id').execute()
-        return file.get('id')
+        mimetype = mimetypes.guess_type(archivo)[0] or 'text/plain'
+        filename = os.path.basename(archivo)
+
+        drive = Drive(self.credentials_file)
+        drive.cd(self.path)
+        drive.write(filename, mimetype, archivo)
